75862d031dc1a624121c63b3dc20af4abf59a7f4
thucvanminh <thucnichan2929vmt@gmail.com>
2023-12-06 12:02:43 +0700

    test2

--- src/testpackk/test.java
+++ src/testpackk/test.java
@@ -9,6 +9,6 @@ public class test {
         Scanner sc = new Scanner (System.in);
         AccountManager acc1 = new AccountManager();
         acc1.doProgram(sc);
-        System.out.println("Minh Thuc dep trai");
+        System.out.println("Test Thanh Cong");
     }
 }
